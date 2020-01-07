9f4ca80a4267a7f5ccd1312e334a330b18859c57
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2020-01-07 14:00:53 +0300

    add first tutorial

---
+++ public/scripts/app.js
@@ -0,0 +1,7 @@
+console.log('App.js is running')
+
+//jsx-template
+
+var template = <p>This is jsx where brighton begins to code for real</p>;
+var appRoot = document.getElementById('app');
+ReactDom.render(template, appRoot);
